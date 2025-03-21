acbe50b994d5ba9d0cb689ec2dbd31ebd70a374b
Andrey Fidrya <findhostcoupons@gmail.com>
2025-03-21 18:08:01 +0200

    adding category from admin panel

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
+use App\Http\Controllers\AdminController;
+
 route::get('/',[HomeController::class,'home']);
 
 Route::get('/dashboard', function () {
@@ -21,3 +23,9 @@ require __DIR__.'/auth.php';
 
 
 route::get('admin/dashboard', [HomeController::class, 'index'])->middleware(['auth', 'admin']);
+
+
+route::get('view_category', [AdminController::class, 'view_category'])->middleware(['auth', 'admin']);
+
+
+route::post('add_category', [AdminController::class, 'add_category'])->middleware(['auth', 'admin']);
