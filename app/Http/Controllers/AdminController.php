839e65b773505ef19a6c515b5c0a99adc7318849
Andrey Fidrya <findhostcoupons@gmail.com>
2025-03-25 17:59:05 +0200

    deleting category

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -31,4 +31,16 @@ class AdminController extends Controller
 
 
     }
+
+
+    public function delete_category($id)
+    {
+        $data = Category::find($id);
+
+        $data->delete();
+
+        toastr()->timeout(10000)->closeButton()->success('Category Deleted Successfully.');
+
+        return redirect()->back();
+    }
 }
