3735778cdec4e4402c1a438d6982ffc51a1d9ca4
Andrey Fidrya <findhostcoupons@gmail.com>
2025-03-25 16:51:59 +0200

    displaying table of categories

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -19,6 +19,32 @@
             margin: 30px;
         }
 
+        .table_deg
+        {
+
+          text-align:center;
+          margin: auto;
+          border: 2px solid yellowgreen;
+          margin-top: 50px;
+          width: 600px;
+        }
+
+        th
+        {
+            background-color: skyblue;
+            padding: 15px;
+            font-size: 20px;
+            font-weight: bold;
+            color:white;  
+        }
+
+        td
+        {
+          color: white;
+          padding: 10px;
+          border: 1px solid skyblue;
+        }
+
     </style>
 
   </head>
@@ -53,8 +79,28 @@
 
                 </form>
 
+                </div>
+
+                <div>
+
+                    <table class="table_deg">
+                        <tr>
+                            <th>Category Name</th>
+                        </tr>
+
+                        @foreach($data as $data)
+
+                        <tr>
+                            <td>{{$data->category_name}}</td>
+                        </tr>
+
+                        @endforeach
+                        
+                    </table>  
+
 
                 </div>
+                
 
 
                 </div>
